817ac0596d4057cf258d7f4c196cea0c0746072b
Microd18 <Microd@inbox.ru>
2023-04-02 00:23:42 +0500

    First commit

---
+++ src/main/java/com/example/colectionsxx/ColectionsXxApplication.java
@@ -0,0 +1,13 @@
+package com.example.colectionsxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColectionsXxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColectionsXxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/colectionsxx/ColectionsXxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colectionsxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColectionsXxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
